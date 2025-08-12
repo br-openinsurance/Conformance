743fa6caed0eef5bd7e92b250344353bf12bf1c5
alexandre-raidiam <alexandre.guimaraes@raidiam.com>
2025-08-11 21:31:55 -0300

    skips old files in table creation

--- conformance_table_generator/utils/generate_table.go
+++ conformance_table_generator/utils/generate_table.go
@@ -1,12 +1,41 @@
 package utils
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/br-openinsurance/Conformance/tree/main/conformance_table_generator/models"
 )
+
+// loadOldFileNames loads the list of old file names from old_file_names.txt
+func loadOldFileNames() map[string]bool {
+	oldFileNames := make(map[string]bool)
+
+	file, err := os.Open("../old_file_names.txt")
+	if err != nil {
+		log.Printf("Warning: Could not open old_file_names.txt: %v", err)
+		return oldFileNames
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fileName := strings.TrimSpace(scanner.Text())
+		if fileName != "" {
+			oldFileNames[fileName] = true
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Printf("Warning: Error reading old_file_names.txt: %v", err)
+	}
+
+	log.Printf("Loaded %d old file names to skip", len(oldFileNames))
+	return oldFileNames
+}
 
 func GenerateTable(apisList []string, phase string, version string) {
 	// import files
@@ -19,18 +48,18 @@ func GenerateTable(apisList []string, phase string, version string) {
 	log.Printf("Found %d files matching the criteria for phase %s and version %s", len(filteredFiles), phase, version)
 
 	// create table and dump
-	tableHeaders := []string {"Organização", "Deployment"}
+	tableHeaders := []string{"Organização", "Deployment"}
 	tableHeaders = append(tableHeaders, apiTableHeaders(apisList)...)
-	table := [][]string {tableHeaders}
+	table := [][]string{tableHeaders}
 
-	dumpHeaders := []string {"Id da Organização", "Deployment", "API", "Version", "Data"}
-	dump := [][]string {dumpHeaders}
+	dumpHeaders := []string{"Id da Organização", "Deployment", "API", "Version", "Data"}
+	dump := [][]string{dumpHeaders}
 
 	organisationsMap := makeOrganisationsMap(false)
 
 	for _, file := range filteredFiles {
 		fileSplit := strings.Split(file, "/")
-		fileName := fileSplit[len(fileSplit) - 1]
+		fileName := fileSplit[len(fileSplit)-1]
 		fileNameSplit := strings.Split(fileName, "_")
 		orgId := fileNameSplit[0]
 		orgName := organisationsMap[orgId]
@@ -43,10 +72,10 @@ func GenerateTable(apisList []string, phase string, version string) {
 		date := strings.Split(fileNameSplit[4], ".")[0]
 
 		zipUrl := strings.Replace(repositoryUrl, "api.github.com/repos", "github.com", 1)
-		zipUrl = strings.Replace(zipUrl, "git/trees/main?recursive=1", "blob/main/" + file, 1)
+		zipUrl = strings.Replace(zipUrl, "git/trees/main?recursive=1", "blob/main/"+file, 1)
 		zipUrl = strings.Replace(zipUrl, " ", "%20", -1)
 
-		dump = append(dump, []string {
+		dump = append(dump, []string{
 			orgId,
 			deploymentName,
 			api,
@@ -59,11 +88,11 @@ func GenerateTable(apisList []string, phase string, version string) {
 			newRow := make([]string, len(tableHeaders))
 			newRow[0] = orgName
 			newRow[1] = deploymentName
-			newRow[apiIndex + 2] = fmt.Sprintf("[%s](%s)", date, zipUrl)
-			
+			newRow[apiIndex+2] = fmt.Sprintf("[%s](%s)", date, zipUrl)
+
 			table = append(table, newRow)
 		} else {
-			table[ind][apiIndex + 2] = fmt.Sprintf("[%s](%s)", date, zipUrl)
+			table[ind][apiIndex+2] = fmt.Sprintf("[%s](%s)", date, zipUrl)
 		}
 	}
 
@@ -77,17 +106,27 @@ func GenerateTable(apisList []string, phase string, version string) {
 func filterFilesByApisAndVersion(submissionFiles models.GithubTree, apisList []string) []string {
 	var filteredFiles []string
 
+	// Load old file names to skip
+	oldFileNames := loadOldFileNames()
+
 	for _, file := range submissionFiles {
 		filePath := file.Path
 		fileSplit := strings.Split(filePath, "/")
 		fileApi := fileSplit[2]
 		fileVersion := fileSplit[3]
 
+		// Skip files that are in the old file names list
+		fileName := fileSplit[len(fileSplit)-1]
+		if oldFileNames[fileName] {
+			log.Printf("Skipping old file: %s", fileName)
+			continue
+		}
+
 		apiIndex := findApiIndex(apisList, translateNameFromFileToApisList(fileApi, fileVersion))
-		
+
 		if apiIndex != -1 {
 			listVersion := strings.Split(apisList[apiIndex], "_")[1][1:]
-			isVersionEqual := fileVersion == listVersion + ".0"
+			isVersionEqual := fileVersion == listVersion+".0"
 			isOldVersion := strings.HasSuffix(listVersion, "-old")
 			isOldVersionEqual := strings.Replace(fileVersion, ".0", "", 1) == listVersion
 
@@ -102,7 +141,7 @@ func filterFilesByApisAndVersion(submissionFiles models.GithubTree, apisList []s
 
 func findApiIndex(apis []string, api string) int {
 	for i, element := range apis {
-		if element == api || element == api + "-old" {
+		if element == api || element == api+"-old" {
 			return i
 		}
 	}
